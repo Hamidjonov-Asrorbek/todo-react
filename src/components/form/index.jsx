a85ce7b31201948853ff7ff4461b696aafa16fde
Hamidjonov-Asrorbek <asrorbekhamidjonov1@gmail.com>
2024-05-03 04:39:02 +0500

    todo

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import Modal from "../Modal";
 import del from '/src/assets/delete.svg';
 import edit from '/src/assets/edit.svg';
 import './style.css'
@@ -7,9 +8,19 @@ import './style.css'
 
 export default function Form(){
   const [todos, setTodos] = useState([]);
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+ }, [todos]);
+  const [modal, setModal] = useState({
+    show: false,
+    todoId: "",
+    todoText: "",
+  });
+  
+  
   // const input = useRef();
-
+  
   // form create
   function FormSubmit(e){
     e.preventDefault();
@@ -19,22 +30,32 @@ export default function Form(){
     let newTodo = {
       id: date.getTime(),
       text,
+      time: getTime(),
     }
-    setTodos((prev) => [...prev, newTodo])
+    setTodos((prev) => [...prev, newTodo]);
+    localStorage.setItem("todos", JSON.stringify(todos));
     setText('');
   }
+
   // ************************
 
   // delete todo
   function deleteTodo(todoId){
-    setTodos((prev) =>{
-      prev.filter((todo) =>{
-        return todo.id !== todoId;
-      })
-    })
+    setTodos((prev) => prev.filter((todo) => todo.id !== todoId))
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
   // ****************
-  console.log(todos);
+  
+  // time
+
+  function getTime(){
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+    const hour = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
+    const minute = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
+    return `${hour}:${minute}, ${date}`;
+}
   
   // html return
   return  (
@@ -57,19 +78,20 @@ export default function Form(){
       </div>
     </form>
   <ul id="list-group-todo" className="list-group d-flex justify-content-between">
-                 {/* todo create */}
                  {todos.length ? 
-                  todos.map(({id, text}) => {
-                    return <li className='list-group-item' key={id}>
+                  todos.map(({id, text, time}) => {
+                    return (<li className='list-group-item' key={id}>
                     <p>{text}</p>
                     <div className='align-items-center gap-5'>
-                        <span className='opacity-50 me-2'></span>
-                        <img src={edit} width='25' style={{marginRight: "10px"}} alt="edit"/>
-                        <img src={del} onClick={() => {deleteTodo(id)}} width='25' alt="delete"/>
+                        <span className='opacity-50 me-2'>{time}</span>
+                        <img src={edit} onClick={() => setModal((prev) =>{
+                          return {...prev, show:true, todoId: id, todoText:text}
+                        })} width='25' style={{marginInline: "10px"}} alt="edit"/>
+                        <img src={del} onClick={() => deleteTodo(id)} width='25' alt="delete"/>
                     </div>
-                  </li>
+                  </li>);
                  }): "No todos :)"}
-                 {/* **************** */}
   </ul>
+  {modal.show && <Modal setTodos = {setTodos} itemId = {modal.todoId} itemText = {modal.todoText} closeModal={setModal}/>}
   </>)
 }
